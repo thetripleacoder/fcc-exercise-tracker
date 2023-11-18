5a37291f59475285216c56534ecdb2083121c612
aldousalde <85655422+thetripleacoder@users.noreply.github.com>
2023-11-18 12:24:02 +0800

    Create Serverless express API to vercel:
    - export app as module
    - create vercel.json file
    
    src: https://shadowsmith.com/thoughts/how-to-deploy-an-express-api-to-vercel

--- index.js
+++ index.js
@@ -140,3 +140,6 @@ app.get('/api/users/:_id/logs', (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//  Export the Express API
+module.exports = app;
